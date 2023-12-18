fdf4c7e3e71882c826f06da17a1531d3a583d4a5
rcruzoliver <raul.cruz.oliver@gmail.com>
2023-12-18 10:34:58 +0100

    initial commit

---
+++ createSnap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+sudo snapcraft clean --destructive-mode
+sudo snapcraft --destructive-mode 
+
+
+
+
+
---
+++ shscripts/run.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#wait thill the solution plug is connected :)
+while ! snapctl is-connected active-solution 
+do 
+  sleep 5 
+done
+
+export LC_ALL=C   #Locale settings Override, MongoDB stuffs
+
+#MyAppPersonaldir ---> Adapt for yourself
+MY_FOLDERNAME=ctrlx-mongodb
+
+
+#whatsMyDirAgain? better not modify this
+MYDIR="$SNAP_COMMON/solutions/activeConfiguration/$MY_FOLDERNAME"
+
+if [ ! -d "$MYDIR" ]; then
+	mkdir $MYDIR
+    echo $MYDIR
+fi
+
+#move myself in the working directory, from here enything is almost untouched
+cd $MYDIR
+
+
+# ensure the conf directory exists
+if [ ! -f "./mongodb.conf" ]; then
+	cp $SNAP/bin/conf/mongodb.conf ./mongodb.conf
+    echo "file conf "
+fi
+
+if [ ! -f "./mongodb.log" ]; then
+	cp $SNAP/bin/conf/mongodb.log ./mongodb.log
+    echo "file "
+fi
+
+if [ ! -d "./mongodb" ]; then
+	mkdir ./mongodb
+    echo "folder "
+fi
+
+exec $SNAP/usr/bin/mongod -f ./mongodb.conf
